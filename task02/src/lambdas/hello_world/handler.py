b7aa5a491900afb9a46df6dd71c3517c42086cf7
Vialeta <vialeta_karpushenka@epam.com>
2025-01-04 22:39:26 +0300

    Task 02

---
+++ task02/src/lambdas/hello_world/handler.py
@@ -0,0 +1,35 @@
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+import logging
+import json
+
+_LOG = get_logger('HelloWorld-handler')
+
+logger = logging.getLogger()
+
+class HelloWorld(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        pass
+        
+    def handle_request(self, event, context):
+        logger.info("event: " + json.dumps(event))
+        path = event['requestContext']['http']['path']
+        method = event['requestContext']['http']['method']
+        if path == '/hello' and method == 'GET':
+            return {
+                'statusCode': 200,
+                'message': 'Hello from Lambda'
+            }
+        else:
+            return {
+                'statusCode': 400,
+                'message': "Bad request syntax or unsupported method. Request path: " + path + ". HTTP method: " +method
+            }
+    
+
+HANDLER = HelloWorld()
+
+
+def lambda_handler(event, context):
+    return HANDLER.lambda_handler(event=event, context=context)
